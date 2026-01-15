9a90f817830c2dfe917163b3046116e8b6c8dd4a
alopgau <alopezg@iesrafaelalberti.es>
2026-01-15 10:24:22 +0100

    Juego a medias 1

--- hundirflota.py
+++ hundirflota.py
@@ -1,11 +1,11 @@
 """TODO: Tablero 8x8. Barcos: 5 (Portaaviones), 4 (Acorazado), 3 (Submarino), 3 (Crucero), 3 (Destructor) 2 Coordenadas (letra,numero), numero: fila, letra, columna"""
-import random
+from random import randint, choice
 
 """
 TODO: 
 --- Funciones mínimas ---
 - colocar_barcos(tablero_jugador1 : list[list[Any]]
-- disparo(tablero,fila,col) : String --> "AGUA" , "TOCADO" o "HUNDIDO"
+- disparar(tablero,fila,col) : String --> "AGUA" , "TOCADO" o "HUNDIDO"
 
 *** A implementar ***
 - El juego se acaba cuando el contador de barcos hundidos sea igual al número de barcos a hundir.
@@ -16,8 +16,22 @@ TODO:
     - O : disparo al agua
 """
 
-def colocar_barcos(tablero: list[list]):
-    b = input("Barco\n")
+def colocar_barcos(tablero: list[list], barcos:dict[int,dict[str,int]]):
+    rango = 5
+    barcos_disponibles = [key for key in barcos.keys()]
+    orientaciones = ["Horizontal, Vertical"]
+    orientacion = orientaciones[randint(0,1)]
+    numero_barco_generado = choice(barcos_disponibles)
+    max_inicio_barco = 8 - barcos[numero_barco_generado][]
+    inicio_barco = randint(0,max_inicio_barco)
+    if orientacion == "Horizontal":
+        fila = randint(0,7)
+        for j in range(inicio_barco,7):
+
+    if orientacion == "Vertical":
+
+def disparar():
+
 
 def main():
     barcos = {
@@ -48,6 +62,7 @@ def main():
         ["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],
         ["~"],["~"],["~"],["~"],["~"],["~"],["~"],["~"],
     ]
+    colocar_barcos(tablero_jugador1,barcos)
 
 if __name__ == "__main__":
     main()
